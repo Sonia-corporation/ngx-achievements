8ddd059d96f912f037569692719091876f59668c
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-07-31 20:51:42 +0200

    chore: remove useless generated code

--- apps/website/src/app/app.component.ts
+++ apps/website/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy
+} from '@angular/core';
 
 @Component({
   selector: 'ngx-achievements-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'website';
 }
