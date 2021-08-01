d39d16e78de78006a106d0a2522392a82450ae1d
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-08-02 00:30:13 +0200

    fix: build compodoc, storybook and the website as expected in the release

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '*.{ts,js}': files => {return [
+    `npm run affected:lint:ts:hook --files=${files.join(',')}`,
+  ]},
+  '*.{md,json,yml,html}': [
+    'npm run lint:workspace',
+    'npm run affected:lint:other',
+  ],
+};
