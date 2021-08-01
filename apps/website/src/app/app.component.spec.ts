3e3a248d51b811fb08a701ba809278b895685535
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-08-01 19:02:12 +0200

    test: add i18n polyfill to fix an issue

--- apps/website/src/app/app.component.spec.ts
+++ apps/website/src/app/app.component.spec.ts
@@ -10,8 +10,8 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const component = fixture.componentInstance;
 
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
