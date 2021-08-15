7859aa71992762f5336d763a794de6c31f76b936
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2021-08-15 12:09:02 +0200

    fix(assets): load the favicon

---
+++ tools/gh-pages/set-assets-urls.js
@@ -0,0 +1,42 @@
+const FS = require(`fs-extra`);
+const _ = require(`lodash`);
+const CHALK = require(`../chalk`);
+const LOGGER = require(`../logger`);
+const CONTEXT = `set-assets-urls`;
+const LOCALES = ['en', 'fr'];
+const ASSETS = ['favicon.ico'];
+const DEPLOY_URL_PREFIX = 'https://sonia-corporation.github.io/ngx-achievements/';
+
+async function setAssetsUrls() {
+  for (const locale of LOCALES) {
+    LOGGER.log(CONTEXT, CHALK.text(`Handling ${CHALK.value(locale)}...`));
+
+    const folderPath = `gh-pages/${locale}`;
+    LOGGER.debug(CONTEXT, CHALK.text(`Reading ${CHALK.value(folderPath)}...`));
+    const dir = await FS.readdir(folderPath);
+    LOGGER.debug(CONTEXT, CHALK.text(`${CHALK.value(folderPath)} read`));
+
+    for (const file of dir) {
+      const filePath = `gh-pages/${locale}/${file}`;
+      LOGGER.debug(CONTEXT, CHALK.text(`Reading ${CHALK.value(filePath)}...`));
+      let updatedContent = await FS.readFile(filePath, {
+        encoding: 'utf8',
+      });
+      LOGGER.debug(CONTEXT, CHALK.text(`${CHALK.value(filePath)} read`));
+
+      LOGGER.debug(CONTEXT, CHALK.text(`Replacing assets for ${CHALK.value(filePath)}...`));
+
+      for (const asset of ASSETS) {
+        updatedContent = _.replace(updatedContent, new RegExp(asset, 'gm'), `${DEPLOY_URL_PREFIX}${asset}`);
+        LOGGER.debug(CONTEXT, CHALK.text(`Asset ${CHALK.value(asset)} replaced`));
+      }
+
+      await FS.writeFile(filePath, updatedContent);
+      LOGGER.debug(CONTEXT, CHALK.text(`Assets replaced for ${CHALK.value(filePath)}`));
+    }
+
+    LOGGER.success(CONTEXT, CHALK.success(`${CHALK.value(locale)} handled`));
+  }
+}
+
+return setAssetsUrls();
