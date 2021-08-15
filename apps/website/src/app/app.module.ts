bfd95996a1130f43356f1de8e09704923f3d235c
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2021-08-15 12:47:23 +0200

    feat(webapp): add a service worker

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ENVIRONMENT } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: ENVIRONMENT.isProduction,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
